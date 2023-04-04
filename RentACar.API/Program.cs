78e0a113f36662ea4d4ed9a9d41cd4a7aff6b7a3
AtakannOzturk <125278779+AtakannOzturk@users.noreply.github.com>
2023-04-04 17:52:21 +0300

    4.04.2023

---
+++ RentACar.API/Program.cs
@@ -0,0 +1,33 @@
+using RentACar.Business.Abstract;
+using RentACar.Business.Concrete;
+using RentACar.DataAccess.Abstract;
+using RentACar.DataAccess.Concrete.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ICarService, CarManager>();
+builder.Services.AddSingleton<ICarDal, EfCarDal>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
